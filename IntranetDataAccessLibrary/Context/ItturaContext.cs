a8d94a545492e14f814011c55d14d7b277ed0eb9
Yad <yadmarzan@hotmail.com>
2023-01-03 23:57:13 +0100

    546546

--- IntranetDataAccessLibrary/Context/ItturaContext.cs
+++ IntranetDataAccessLibrary/Context/ItturaContext.cs
@@ -100,7 +100,7 @@ public partial class ItturaContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=TUF;Initial Catalog=ITTura;Integrated Security=SSPI;Trusted_Connection=Yes;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Data Source=YAD;Initial Catalog=ITTura;Integrated Security=SSPI;Trusted_Connection=Yes;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
