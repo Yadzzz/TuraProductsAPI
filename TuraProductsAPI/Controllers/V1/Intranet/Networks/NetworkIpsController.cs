8d480e90ddcd5b7522088ad4a2c62e74e01d4d76
Yad <yadmarzan@hotmail.com>
2023-01-06 02:26:34 +0100

    897798

---
+++ TuraProductsAPI/Controllers/V1/Intranet/Networks/NetworkIpsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using IntranetDataAccessLibrary.Context;
+using IntranetDataAccessLibrary.Models;
+
+namespace TuraProductsAPI.Controllers.V1.Intranet.Networks
+{
+    [Route("api/v1/intranet/networks/[controller]")]
+    [ApiController]
+    public class NetworkIpsController : ControllerBase
+    {
+        private readonly ItturaContext _context;
+
+        public NetworkIpsController(ItturaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/NetworkIps
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<NetworkIp>>> GetNetworkIps()
+        {
+          if (_context.NetworkIps == null)
+          {
+              return NotFound();
+          }
+            return await _context.NetworkIps.ToListAsync();
+        }
+
+        // GET: api/NetworkIps/5
+        [HttpGet("{networkId}")]
+        public async Task<ActionResult<List<NetworkIp>>> GetNetworkIp(int networkId)
+        {
+          if (_context.NetworkIps == null)
+          {
+              return NotFound();
+          }
+            var networkIp = await _context.NetworkIps.Where(x => x.NetworkId == networkId).ToListAsync();
+
+            if (networkIp == null)
+            {
+                return NotFound();
+            }
+
+            return networkIp;
+        }
+
+        // PUT: api/NetworkIps/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutNetworkIp(int id, NetworkIp networkIp)
+        {
+            if (id != networkIp.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(networkIp).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!NetworkIpExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/NetworkIps
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<NetworkIp>> PostNetworkIp(NetworkIp networkIp)
+        {
+          if (_context.NetworkIps == null)
+          {
+              return Problem("Entity set 'ItturaContext.NetworkIps'  is null.");
+          }
+            _context.NetworkIps.Add(networkIp);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetNetworkIp", new { id = networkIp.Id }, networkIp);
+        }
+
+        // DELETE: api/NetworkIps/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteNetworkIp(int id)
+        {
+            if (_context.NetworkIps == null)
+            {
+                return NotFound();
+            }
+            var networkIp = await _context.NetworkIps.FindAsync(id);
+            if (networkIp == null)
+            {
+                return NotFound();
+            }
+
+            _context.NetworkIps.Remove(networkIp);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool NetworkIpExists(int id)
+        {
+            return (_context.NetworkIps?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ TuraProductsAPI/Controllers/V1/Intranet/Networks/NetworksController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using IntranetDataAccessLibrary.Context;
+using IntranetDataAccessLibrary.Models;
+
+namespace TuraProductsAPI.Controllers.V1.Intranet.Networks
+{
+    [Route("api/v1/intranet/networks/[controller]")]
+    [ApiController]
+    public class NetworksController : ControllerBase
+    {
+        private readonly ItturaContext _context;
+
+        public NetworksController(ItturaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Networks
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Network>>> GetNetworks()
+        {
+          if (_context.Networks == null)
+          {
+              return NotFound();
+          }
+            return await _context.Networks.ToListAsync();
+        }
+
+        // GET: api/Networks/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Network>> GetNetwork(int id)
+        {
+          if (_context.Networks == null)
+          {
+              return NotFound();
+          }
+            var network = await _context.Networks.FindAsync(id);
+
+            if (network == null)
+            {
+                return NotFound();
+            }
+
+            return network;
+        }
+
+        // PUT: api/Networks/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutNetwork(int id, Network network)
+        {
+            if (id != network.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(network).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!NetworkExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Networks
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Network>> PostNetwork(Network network)
+        {
+          if (_context.Networks == null)
+          {
+              return Problem("Entity set 'ItturaContext.Networks'  is null.");
+          }
+            _context.Networks.Add(network);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetNetwork", new { id = network.Id }, network);
+        }
+
+        // DELETE: api/Networks/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteNetwork(int id)
+        {
+            if (_context.Networks == null)
+            {
+                return NotFound();
+            }
+            var network = await _context.Networks.FindAsync(id);
+            if (network == null)
+            {
+                return NotFound();
+            }
+
+            _context.Networks.Remove(network);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool NetworkExists(int id)
+        {
+            return (_context.Networks?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
